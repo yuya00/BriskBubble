7b8722c7288faf50295b8ded1ad5ded25da96881
yuya <yuya.0810@ezweb.ne.jp>
2019-10-23 10:26:36 +0900

    整理

--- Assets/Chara/CharaBase.cs
+++ Assets/Chara/CharaBase.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class CharaBase : MonoBehaviour {
 
-	protected Rigidbody rigid;
-	protected Vector3 velocity;                 //速さ(rigd.velocityでも良いかも)
-	public float run_spd = 15.0f;   //通常の速さ
-    public float walk_spd = 3.0f;    //歩きの速さ
-    public float jump_power = 15.0f;    //ジャンプ力
-	public float stop_fric = 0.3f;  //慣性(停止)
-	protected float jump_fric = 0;      //慣性(ジャンプ)
-	protected float jump_fric_power = 0.7f;    //慣性(ジャンプ)
-	protected bool is_ground = false;   //地面接地判定
-	protected Transform chara_ray;                  //レイを飛ばす位置(地面判別に使用)
-	public float chara_ray_length = 2f;      //レイの距離
-	public float gravity_power = 5;       //重力の倍率
-	protected int[] iwork = new int[8];     //汎用
-	protected float[] fwork = new float[8];     //汎用
+	protected			 Rigidbody rigid;
+	protected			 Vector3 velocity;                 //速さ(rigd.velocityでも良いかも)
+	public float		 run_spd = 15.0f;   //通常の速さ
+    public float		 walk_spd = 3.0f;    //歩きの速さ
+    public float		 jump_power = 15.0f;		//ジャンプ力
+	public float		 stop_fric = 0.3f;			//慣性(停止)
+	protected float		 jump_fric = 0;				//慣性(ジャンプ)
+	protected float		 jump_fric_power = 0.7f;    //慣性(ジャンプ)
+	protected bool		 is_ground = false;			//地面接地判定
+	protected			 Transform chara_ray;       //レイを飛ばす位置(地面判別に使用)
+	public float		 chara_ray_length = 2f;		//レイの距離
+	public float		 gravity_power = 5;			//重力の倍率
+	protected int[]		 iwork = new int[8];		//汎用
+	protected float[]	 fwork = new float[8];		//汎用
 
 
 	// Start is called before the first frame update
