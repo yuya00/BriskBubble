40e19716be5c6b0d0b25eb4307ded988e351c3e8
Yu-suke <recruitimaoka@gmail.com>
2019-11-06 10:40:42 +0900

    yusuke、ショット消去追加、演出追加

--- Assets/Productions/Fade/Scripts/Fade_Test.cs
+++ Assets/Productions/Fade/Scripts/Fade_Test.cs
@@ -5,28 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class Fade_Test : MonoBehaviour
 {
-    public Fade fade;
-    private float interval_time = 0.0f;
-    public string scene_name;
-    public float interval_time_max;
-    private bool fade_fg = false;
+    public  Fade    fade;
+    public  string  scene_name;
+    private bool    fade_fg = false;
+    private float   interval_time = 0.0f;
+    public  float   interval_time_max;
+
+    public float fade_in_time = 0.5f;
+    public float fade_out_time = 1.0f;
+
 
     void Start()
     {
+        fade.FadeOut(fade_out_time);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            fade.FadeIn(0.5f, () => print("フェードイン完了"));
-            fade_fg = true;
+            fade.FadeIn(fade_in_time);
+            //fade_fg = true;
         }
         else if (Input.GetKeyDown(KeyCode.X))
         {
-            fade.FadeOut(0.5f, () => print("フェードアウト完了"));
-            //TransScene(scene_name, interval_time_max);
+            fade.FadeOut(fade_out_time);
         }
+
         if (fade_fg) TransScene(scene_name, interval_time_max);
     }
 
@@ -36,10 +41,15 @@ public class Fade_Test : MonoBehaviour
         interval_time += Time.deltaTime;
         if (interval_time > interval_time_max)
         {
-            //シーン切替
-            SceneManager.LoadScene(scene_name);
+            scene_change(scene_name);
             fade_fg = false;
         }
+    }
+
+    //シーン切替
+    void scene_change(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
     }
 
 }
