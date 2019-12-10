a913637edbc5de32f3e462ada76765b9583a6ca6
birdjisus <birdjisus@gmail.com>
2019-12-11 01:50:41 +0900

    ショットの追加（バグ多いのでほぼコメント化）

--- Assets/Player/Shots/Shot03.cs
+++ Assets/Player/Shots/Shot03.cs
@@ -9,6 +9,19 @@ public sealed class Shot03 : ShotBase
     {
         base.Start();
         speed += player.GetComponent<Player>().RunSpeed * PLR_SPD;
+
+        //rigid.AddForce(new Vector3(10, 30, 10), ForceMode.Impulse);
+
+        //着地地点の座標
+        //Vector3 target_pos = forward * 20;
+
+        ////射出角度
+        //float angle = 45.0f;
+
+        //Vector3 velocity = CalVelocity(rigid.position,target_pos,angle);
+
+        //rigid.AddForce(velocity, ForceMode.Impulse);
+
     }
 
     // Update is called once per frame
@@ -28,5 +41,24 @@ public sealed class Shot03 : ShotBase
         // 速度を落とす
         if (SpeedDownCheck(spd_down_timer_max)) Down(down_spd, down_pos);
     }
+
+    //private Vector3 CalVelocity(Vector3 pointA, Vector3 pointB, float angle)
+    //{
+    //    //角度をラジアンに変換
+    //    float rad = angle * Mathf.PI / 180;
+
+    //    //水平方向の距離x
+    //    float x = Vector2.Distance(new Vector2(pointA.x, pointA.z), new Vector2(pointB.x, pointB.z));
+
+    //    // 垂直方向の距離y
+    //    float y = pointA.y - pointB.y;
+
+    //    // 斜方投射の公式
+    //    float speed = Mathf.Sqrt(-Physics.gravity.y * Mathf.Pow(x, 2) / (2 * Mathf.Pow(Mathf.Cos(rad), 2) * (x * Mathf.Tan(rad) + y)));
+
+    //    return (new Vector3(pointB.x - pointA.x, x * Mathf.Tan(rad), pointB.z - pointA.z).normalized * speed);
+
+
+    //}
 
 }
