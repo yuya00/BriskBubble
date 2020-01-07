21394a81a29987c0b2a6d1dee46a08a35eabcb3d
birdjisus <birdjisus@gmail.com>
2020-01-08 01:40:36 +0900

    チュートリアル途中

---
+++ Assets/Tutorial/Script/ImageSetPos.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageSetPos : MonoBehaviour
+{
+
+    private Transform camera_pos;
+    private Transform my_transform;
+    public GameObject camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        my_transform = GetComponent<Transform>();
+
+        my_transform.position = GameObject.Find("Signboard").transform.position;
+
+        camera = GameObject.Find("Camera");
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        my_transform.LookAt(camera.transform);
+    }
+}
---
+++ Assets/Tutorial/Script/Signboard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Signboard : MonoBehaviour
+{
+
+    public GameObject signboard_object = null;
+    public Image signboard_image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        signboard_object = GameObject.Find("test_image");
+        signboard_image = signboard_object.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        signboard_image.enabled = false;
+    }
+}
