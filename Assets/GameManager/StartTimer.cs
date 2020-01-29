1283585d07c997de638efda3b71d6b4bcaf28eff
Yu-suke <recruitimaoka@gmail.com>
2020-01-29 21:12:48 +0900

    UI大きさ

--- Assets/GameManager/StartTimer.cs
+++ Assets/GameManager/StartTimer.cs
@@ -9,17 +9,24 @@ public class StartTimer : MonoBehaviour
     private Image image;
     private Sprite sprite;
 
+    private Vector3 init;
+    private Vector3 init_scale;
+
     public string path = "red";
 
     void Start()
     {
         obj = GameObject.FindGameObjectWithTag("GameManager");
+        init = transform.position;
+        init_scale = transform.localScale;
     }
 
     void Update()
     {
+
         // 表示する数字を取得
         int sec = obj.GetComponent<Scene>().Send_buf_no;
+        Scale(sec);
 
         // 描画
         if (sec < 4)
@@ -31,6 +38,14 @@ public class StartTimer : MonoBehaviour
 
         // いらなくなったら削除
         if (sec <= 0) Destroy(gameObject);
+
+        transform.position = init;
+    }
+
+    void Scale(int sec)
+    {
+        float[] data = { 1, 2.5f, 2, 1.5f, 1 };
+        transform.localScale = init_scale / data[sec];
     }
 
     // 透明度調整
